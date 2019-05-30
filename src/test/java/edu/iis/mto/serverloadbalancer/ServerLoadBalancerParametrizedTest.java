7b024511c60398a98cb6649af00f2a4befb934d2
Ernest Figura <209308@edu.p.lodz.pl>
2019-05-30 17:41:57 +0200

    Add Parametrized Test

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerParametrizedTest.java
@@ -6,12 +6,34 @@ import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class ServerLoadBalancerParametrizedTest extends ServerLoadBalancerBaseTest{
+
+
+	@Parameterized.Parameters
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] {
+				 { 1, 1 }, { 2, 2}, { 3, 3 }, { 4, 4 }, { 5, 5 }, { 6, 6 }, {7, 7}, {8, 8}, {9, 9}, {10, 10}
+		});
+	}
+
+	@Parameterized.Parameter // first data value (0) is default
+	public /* NOT private */ int amountOfVM;
+
+	@Parameterized.Parameter(1)
+	public /* NOT private */ int serverCapacity;
+
+
 	@Test
 	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm() {
-		Server theServer = a(server().withCapacity(1));
-		Vm theVm = a(vm().ofSize(1));
+		Server theServer = a(server().withCapacity(serverCapacity));
+		Vm theVm = a(vm().ofSize(amountOfVM));
 		balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
 
 		assertThat(theServer, hasLoadPercentageOf(100.0d));
