52c8c5bdf53d4df6c688dc49f79e52d0454b52cd
Zandros229 <ernest.figura548@gmail.com>
2019-06-25 18:27:49 +0200

    implements load Percentage of Server with zero Vms add files ...

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -0,0 +1,31 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
+    private double expectedLoadPercentage;
+
+    public CurrentLoadPercentageMatcher(double expectedLoadPercentage) {
+        this.expectedLoadPercentage = expectedLoadPercentage;
+    }
+
+    @Override
+    protected boolean matchesSafely(Server server) {
+        return doublesAreEqual(this.expectedLoadPercentage, server.currentLoadPercentage);
+    }
+
+    private boolean doublesAreEqual(double d1, double d2) {
+        return d1 == d2 || Math.abs(d1 - d2)<0.01d;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("a server with Load percentage of ").appendValue(expectedLoadPercentage);
+    }
+
+    public static CurrentLoadPercentageMatcher hasCurrentLoad(double expectedLoadPercentage) {
+        return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
+    }
+}
