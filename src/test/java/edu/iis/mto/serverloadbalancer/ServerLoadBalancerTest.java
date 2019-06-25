43d68250ea0a071da789621e591f3cbd4e577c81
Zandros229 <ernest.figura548@gmail.com>
2019-06-25 18:27:24 +0200

    implements load Percentage of Server with zero Vms

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,16 +1,49 @@
 package edu.iis.mto.serverloadbalancer;
 
 
+import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasCurrentLoad;
+import static edu.iis.mto.serverloadbalancer.ServerBuilder.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.hamcrest.Matcher;
 import org.junit.Test;
+
+import javax.management.MBeanServerBuilder;
 
 public class ServerLoadBalancerTest {
 	@Test
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
 	}
+
+	@Test
+	public void balancingServerWithNoVms_serverStaysEmpty(){
+		Server theServer=a(server().withCapacity(1));
+
+		balancing(aServersListWith(theServer), aEmptyListOfVms());
+
+		assertThat(theServer, hasCurrentLoad(0.0d));
+	}
+
+
+
+	private void balancing(Server[] servers, Vm[] vms) {
+		new ServerLoadBalancer().balance(servers,vms);
+	}
+
+	private Vm[] aEmptyListOfVms() {
+		return new Vm[0];
+	}
+
+	private Server[] aServersListWith(Server... servers) {
+		return servers;
+	}
+
+	private Server a(ServerBuilder builder) {
+		return builder.build();
+	}
+
 
 
 }
