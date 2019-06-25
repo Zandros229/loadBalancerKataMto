52d97af71d8e7585914bbb79283351cd0044dabc
Zandros229 <ernest.figura548@gmail.com>
2019-06-25 19:23:30 +0200

    implements server load with two vms and as always add file

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerVmsCountMatch.java
@@ -0,0 +1,29 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class ServerVmsCountMatch extends TypeSafeMatcher<Server> {
+    private int expectedVmsCount;
+
+    public ServerVmsCountMatch(int expectedVmsCount) {
+        this.expectedVmsCount = expectedVmsCount;
+    }
+
+    @Override
+    protected boolean matchesSafely(Server server) {
+        return expectedVmsCount==server.countVms();
+    }
+
+    @Override
+    protected void describeMismatchSafely(Server item, Description description) {
+        description.appendText("a server with vms count of ").appendValue(item.countVms());
+    }
+
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("a server with vms count of ").appendValue(expectedVmsCount);
+    }
+}
