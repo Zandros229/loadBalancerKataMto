414de101854f93080a70ffd60e49a47b4ebd0701
Zandros229 <ernest.figura548@gmail.com>
2019-06-26 13:45:35 +0200

    add files

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerVmsCountMatcher.java
@@ -0,0 +1,28 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class ServerVmsCountMatcher extends TypeSafeMatcher<Server> {
+    private int expectedAmountOfVms;
+
+    public ServerVmsCountMatcher(int expectedAmountOfVms) {
+        this.expectedAmountOfVms = expectedAmountOfVms;
+    }
+
+    @Override
+    protected boolean matchesSafely(Server item) {
+        return expectedAmountOfVms==item.countOfVms();
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("a server with vms count of ").appendValue(expectedAmountOfVms);
+    }
+
+    @Override
+    protected void describeMismatchSafely(Server item, Description description) {
+        description.appendText("a server with vms count of ").appendValue(item.countOfVms());
+    }
+}
