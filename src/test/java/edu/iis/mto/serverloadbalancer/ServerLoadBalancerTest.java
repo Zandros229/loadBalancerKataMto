d47f99b50e1b1a1279ad991e3872d552255a17b7
Zandros229 <ernest.figura548@gmail.com>
2019-06-27 15:53:18 +0200

    refactor extract to method and set fabric method into classes that they creat

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,6 +1,8 @@
 package edu.iis.mto.serverloadbalancer;
 
 
+import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
+import static edu.iis.mto.serverloadbalancer.ServerLoadPercentageMatcher.hasCurrentLoadOf;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -21,9 +23,7 @@ public class ServerLoadBalancerTest {
 	    assertThat(theServer, hasCurrentLoadOf(0.0d));
     }
 
-    private Matcher<? super Server> hasCurrentLoadOf(double expectedLoadPercentage) {
-        return new ServerLoadPercentageMatcher(expectedLoadPercentage);
-    }
+
 
     private void balance(Server[] servers, Vm[] vms) {
         new ServerLoadBalancer().balance(servers,vms);
@@ -41,8 +41,6 @@ public class ServerLoadBalancerTest {
         return builder.build();
     }
 
-    private ServerBuilder server() {
-        return new ServerBuilder();
-    }
+
 
 }
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadPercentageMatcher.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadPercentageMatcher.java
@@ -1,18 +1,27 @@
 package edu.iis.mto.serverloadbalancer;
 
 import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
 
 public class ServerLoadPercentageMatcher extends TypeSafeMatcher<Server> {
+    public static final double EPSILON = 0.01d;
     private double expectedLoadPercentage;
 
     public ServerLoadPercentageMatcher(double expetedLoadPercentage) {
         this.expectedLoadPercentage = expetedLoadPercentage;
     }
+    public static ServerLoadPercentageMatcher hasCurrentLoadOf(double expectedLoadPercentage) {
+        return new ServerLoadPercentageMatcher(expectedLoadPercentage);
+    }
 
     @Override
     protected boolean matchesSafely(Server item) {
-        return expectedLoadPercentage == item.currentLoadPercentage || Math.abs(expectedLoadPercentage-item.currentLoadPercentage)<=0.01d;
+        return areDoublesEqual(this.expectedLoadPercentage, item.currentLoadPercentage);
+    }
+
+    private boolean areDoublesEqual(double d1, double d2) {
+        return d1 == d2 || Math.abs(d1 - d2) <= EPSILON;
     }
 
     @Override
