847e3bf16b43a7c5fda6bf440cf3cdbc7e949bc0
Zandros229 <ernest.figura548@gmail.com>
2019-06-27 16:00:43 +0200

    refactor create interface Builder

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -3,6 +3,7 @@ package edu.iis.mto.serverloadbalancer;
 
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static edu.iis.mto.serverloadbalancer.ServerLoadPercentageMatcher.hasCurrentLoadOf;
+import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -39,13 +40,8 @@ public class ServerLoadBalancerTest {
         return vms;
     }
 
-    private Vm a(VmBuilder builder) {
-        return builder.build();
-    }
 
-    private VmBuilder vm() {
-        return new VmBuilder();
-    }
+
 
 
     private void balance(Server[] servers, Vm[] vms) {
@@ -60,9 +56,10 @@ public class ServerLoadBalancerTest {
         return servers;
     }
 
-    private Server a(ServerBuilder builder) {
-        return builder.build();
+    private <T> T a(Builder<T> builder){
+	    return builder.build();
     }
+
 
 
 
